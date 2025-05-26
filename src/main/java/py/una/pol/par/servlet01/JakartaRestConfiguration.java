064a19a1ecb099ad2b9abb32edc6aa6662165dfd
Juan Delvalle <acx.jose@fpuna.edu.py>
2025-05-26 15:17:50 -0300

    Commit inicial del proyecto

---
+++ src/main/java/py/una/pol/par/servlet01/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package py.una.pol.par.servlet01;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
